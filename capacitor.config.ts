ca443ade8c6b6f3d9cfdc830fb2e25ddb8d7c961
Anthony Destenay <anthony.destenay@gmail.com>
2026-07-10 09:05:28 +0200

    Added Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thoanny.vestigia',
+  appName: 'Vestigia',
+  webDir: 'dist'
+};
+
+export default config;
